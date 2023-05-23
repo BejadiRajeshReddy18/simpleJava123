b4ab7a3c5db26ca759cbaa2fae12c6f716fa633d
RajeshReddy <bejadirajeshreddy@gmail.com>
2023-05-23 05:50:16 +0530

    c6

--- Example.java
+++ Example.java
@@ -1,12 +1,9 @@
-public class Example
-{
-	public static void main(string[] args)
-	{
-		System.out.println("Hello World!");
-		System.out.println("200 lines of data");
-		System.out.println("500 lines of data");
-		 System.out.println("1000 lines of data");
-	}
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("200 lines of data");
+        System.out.println("500 lines of data");
+        System.out.println("1000 lines of data");
+    }
 }
-
 
